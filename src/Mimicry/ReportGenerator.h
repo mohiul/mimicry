3b7250502969ecfcb080fff47b13cfa9bd015500
Mohiul Islam <mohiul@users.noreply.github.com>
2009-11-03 22:25:50 +0000

    Report generator file name changed

---
+++ src/Mimicry/ReportGenerator.h
@@ -0,0 +1,13 @@
+#ifndef REPORT_GENERATOR_H
+#define REPORT_GENERATOR_H
+
+#include "CAPattern.h"
+
+class ReportGenerator
+{
+public:
+	int calculateHammingDistance(CAPattern pattern1, CAPattern pattern2);
+private:
+};
+
+#endif
