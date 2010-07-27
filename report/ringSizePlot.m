7df004a0576ae75e05912628a9cedecc184791f9
mohiul <mohiul@7f6debb6-1952-49e2-8869-ad29a382276e>
2010-07-27 16:53:43 +0000

    Ring size plot has been added

---
+++ report/ringSizePlot.m
@@ -0,0 +1,11 @@
+clear;
+load rings.dat
+
+T = rings(:, 1);
+R = rings(:, 2);
+%Plot ring size
+plot(T, R, '-k');
+axis([0 T(size(T, 1)) 0 R(size(R, 1))+2 ]);
+xlabel('Simulation Time');
+ylabel('Ring size');
+title('Mimicry ring size variation');
