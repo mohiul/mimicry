220bd0a8bc4a1cb2fc63a55ee1ebf935bb60903c
Mohiul Islam <mohiul@users.noreply.github.com>
2010-12-05 05:55:50 +0000

    New image update.

--- report/ringSizePlot.m
+++ report/ringSizePlot.m
@@ -5,7 +5,8 @@ T = rings(:, 1);
 R = rings(:, 2);
 %Plot ring size
 plot(T, R, '-k');
+%axis([0 T(size(T, 1)) -1 25 ]);
 axis([0 T(size(T, 1)) 0 R(size(R, 1))+2 ]);
-xlabel('Simulation Time');
-ylabel('Number of Rings');
-title('Mimicry number of ring variation');
+xlabel('Simulation Time','fontsize',14);
+ylabel('Number of Rings','fontsize',14);
+title('Mimicry number of ring variation','fontsize',14);
